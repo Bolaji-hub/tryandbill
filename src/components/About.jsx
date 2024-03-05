5b98f43f67395f4fec48d8c24c69b2a76d381206
Barakat Bolaji <barakatbolaji65@gmail.com>
2024-03-05 15:11:54 +0100

    - Implement the about us page - Styling the about us paage

---
+++ src/components/About.jsx
@@ -0,0 +1,22 @@
+const About = () => {
+  return (
+    <div className=" flex flex-col justify-center items-center text-center">
+      <div>
+        <h5 className="text-[#382660] mt-6">About Us</h5>
+        <h1 className="text-xl font-bold">What We Do.</h1>
+        <hr className="w-[50px]" />
+        <p className="px-8 mt-6 mb-4 text-[#454545] text-sm">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
